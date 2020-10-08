7f214360300ce751f16bc0053e00bfd5c3ad33e5
osidekyle <hawkinskyle67@gmail.com>
2020-10-07 22:48:53 -0700

    just about to make graphql call to get image data\

---
+++ gatsby-practice/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import {graphql, useStaticQuery} from "gatsby"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  //const data=useStaticQuery(graphql`)
+  <Layout>
+  <p>Add code</p>
+  </Layout>
+)
+
+export default IndexPage
